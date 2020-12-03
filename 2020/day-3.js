24da472878c96c5f9170b36ccee634fc25b32c2b
Le Tom <zaku@Les-MacBook-Pro.local>
2020-12-03 19:05:19 +0800

    [Day-03] Add js

--- 2020/day-3.js
+++ 2020/day-3.js
@@ -1,20 +1,17 @@
 const { getInput } = require('../helper/input')
 
-const countTree = (map, jump, position) => {
+const countTree = (map, jump, position = [jump.bottom, jump.right]) => {
   if (position[0] >= map.length) return 0
   const nextPosition = [position[0] + jump.bottom, (position[1] + jump.right) % (map[0].length)]
   if (map[position[0]][position[1]] === '#') return 1 + countTree(map, jump, nextPosition)
   return countTree(map, jump, nextPosition)
 }
 
-const productTree = (data, jumps) => {
-  const result = jumps.map(([right, bottom]) => countTree(data, { right, bottom }, [bottom, right])).reduce((acc, item) => acc * item, 1)
-  return result
-}
+const productTree = (data, jumps) => jumps.map(([right, bottom]) => countTree(data, { right, bottom })).reduce((acc, item) => acc * item, 1)
 
 const main = async () => {
   const data = await getInput()
-  console.log(countTree(data, { right: 3, bottom: 1 }, [1, 3]))
+  console.log(countTree(data, { right: 3, bottom: 1 }))
   console.log(productTree(data, [[1, 1], [3, 1], [5, 1], [7, 1], [1, 2]]))
 }
 
